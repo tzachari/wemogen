5cf501f079701f6aa8e0822a6ee68c5f0c45d5c1
Thomas Zachariah <tzachari@umich.edu>
2016-03-31 23:24:46 -0400

    Better mdns and timer support

--- serve.js
+++ serve.js
@@ -3,61 +3,73 @@ var mdns    = require('mdns');
 var wemo    = require('wemo-client');
 var app     = express();
 
-var delay   = 6*60*1000; // Delay for automatic shutoff (6 mins)
+var delay   = 5*60*1000; // Delay for automatic shutoff (6 mins)
 var timeout = null;
-
 
 // Serve www/
 app.use(express.static(__dirname + '/www'));
-app.listen(0,function(){
-  console.log(new Date(),'Serving http://localhost:' + this.address().port);
+app.listen(0, function() {
+  console.log(new Date(), 'Serving http://localhost:' + this.address().port);
 
   // Advertise over mDNS
-  mdns.createAdvertisement(
-  	mdns.tcp('http'),
-    this.address().port,
-    {name:'Wemo Switch'}
-  ).start();
+  mdns.createAdvertisement(mdns.tcp('http'), this.address().port,{name:'WeMo'})
+    .start();
 
   // Discover WeMos & bind events
-  (new wemo()).discover(function(device){
+  (new wemo()).discover(function(device) {
     if (device.deviceType === wemo.DEVICE_TYPE.Switch) {
       console.log(new Date(),'Wemo Switch found: ' + device.friendlyName);
 
       var state   = false;
       var present = false;
       var client  = this.client(device);
+      
+      var seq     = [
+        mdns.rst.DNSServiceResolve(),
+        'DNSServiceGetAddrInfo' in mdns.dns_sd ? 
+          mdns.rst.DNSServiceGetAddrInfo() : 
+          mdns.rst.getaddrinfo({families:[0]}),
+        mdns.rst.makeAddressesUnique()
+      ];
 
       var toggle  = function(req, res) {
         if (req) res.send(!state);
         client.setBinaryState(+!state);
       }
 
+      var timer   = function() {
+        if (timeout) clearTimeout(timeout);
+        timeout = setTimeout(toggle, delay);
+      }
+
       // The switch changed its state
       client.on('binaryState', function(value) {
-        state = (value=='1');
-        if (state & !present) timeout = setTimeout(toggle, delay);
+        state = (value == '1');
+        if (state & !present) timer();
       });
 
       // Detect phone presence over mDNS
-      mdns.createBrowser(mdns.tcp('apple-mobdev2')).on('serviceUp', function(){
-        console.log(new Date(),'Present');
-        present = true;
-        clearTimeout(timeout);
-      }).on('serviceDown', function() {
-        console.log(new Date(),'Absent');
-        present = false;
-        if (state) timeout = setTimeout(toggle, delay);
-      }).start();
+      mdns.createBrowser(mdns.tcp('apple-mobdev2'), {resolverSequence:seq})
+        .on('serviceUp', function() {
+          console.log(new Date(), 'Present');
+          present = true;
+          clearTimeout(timeout);
+        }).on('serviceDown', function() {
+          console.log(new Date(), 'Absent');
+          present = false;
+          if (state) timer();
+        }).on('error', function(e) {
+          console.log(e);
+        }).start();
 
       // Toggle switch
       app.get('/toggle', toggle);
 
       // Get state
-      app.get('/state',function(req, res) {
+      app.get('/state', function(req, res) {
         res.send(state);
       });
-
+      
     }
   });
 
